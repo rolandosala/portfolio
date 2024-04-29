1d8f561b375e715f8dbfcaa0b623245e8820176d
Roland <rolandosalajr@gmail.com>
2024-04-29 19:56:45 +0800

    First Upload Portfolio

---
+++ content.js
@@ -0,0 +1,59 @@
+const knowledge = [
+    {
+        title: 'Cloud Computing',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate optio animi asperiores nam sed quis dolore.',
+        list: ['Google Cloud Platform', 'Firebase']
+    },
+    {
+        title: 'Front-End Development',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate optio animi asperiores nam sed quis dolore.',
+        list: ['HTML, CSS and Javascript', 'Vue.js', 'Bootstrap', 'JQuery']
+    },
+    {
+        title: 'Backend-End Development',
+        description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate optio animi asperiores nam sed quis dolore.',
+        list: ['Node.js', 'Express.js', 'PHP', 'MySQL']
+    },
+];
+
+const about_me = [
+    {
+        title: 'About me',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid nemo consequatur hic in exercitationem corporis, asperiores voluptatibus adipisci, minima aperiam odio dolore. Cum quasi hic nostrum quisquam quia iusto amet! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, reprehenderit adipisci ad, quaerat quia temporibus dolorem enim a voluptates aliquam amet sed dolore voluptate, illum nihil debitis exercitationem officia incidunt.'
+    }
+];
+
+const certifications = [
+    {
+        link: 'cert.html',
+        title: 'Basic Cloud Computing'
+    },
+    {
+        link: '',
+        title: 'Software Engineering'
+    },
+    {
+        link: '',
+        title: 'Software Engineering'
+    },
+    {
+        link: '',
+        title: 'Software Engineering'
+    },
+    {
+        link: '',
+        title: 'Software Engineering'
+    },
+    {
+        link: '',
+        title: 'Software Engineering'
+    },
+    {
+        link: '',
+        title: 'Software Engineering'
+    },
+    {
+        link: '',
+        title: 'Software Engineering'
+    }
+]
---
+++ script.js
@@ -0,0 +1,83 @@
+let btn_showmenu = document.getElementById('btn_showmenu');
+let btn_hidemenu = document.getElementById('btn_hidemenu');
+let sidebar = document.querySelector('.sidebar');
+let content = document.querySelector('.content');
+let menu_list = document.querySelectorAll('.menu_list');
+window.onclick = (event) => {
+    if (event.target == btn_showmenu) {
+        sidebar.style.left = '0%';
+        showElement(btn_hidemenu);
+        hideElement(btn_showmenu);
+    } else if (event.target == btn_hidemenu) {
+        sidebar.style.left = '-100%';
+        showElement(btn_showmenu);
+        hideElement(btn_hidemenu);
+    } else if (event.target == menu_list[0]) {
+        content.innerHTML = `
+        <p class="title">${about_me[0].title}</p>
+        <p>${about_me[0].description}</p>`;
+    } else if (event.target == menu_list[1]) {
+        content.innerHTML = ` 
+        <p class="title">projects</p>
+        <div class="project-container">
+            <div class="project-box">
+                <div class="project-item"></div>
+                <div class="project-item"></div>
+                <div class="project-item"></div>
+                <div class="project-item"></div>
+                <div class="project-item"></div>
+                <div class="project-item"></div>
+            </div>
+        </div>`;
+    } else if (event.target == menu_list[2]) {
+        content.innerHTML = `
+        <p class="title">tech knowledge</p>
+        <div class="project-container">
+            <div class="card-box"></div>
+        </div>`;
+        let card_box = document.querySelector('.card-box');
+        for (let i = 0; i < knowledge.length; i++) {
+           let text = '';
+            for (let x = 0; x < knowledge[i].list.length; x++) {
+                text += `<li>${knowledge[i].list[x]}</li>`;
+            }
+            card_box.innerHTML += `
+                    <div class="card-sm">
+                        <h2>${knowledge[i].title}</h2>
+                        <hr>
+                        <p>${knowledge[i].description}</p>
+                        
+                        <ul class="knowledge-list">
+                            ${text}
+                        </ul>
+                    </div>`;
+        }
+    } else if(event.target == menu_list[3]){
+        content.innerHTML = `
+        <p class="title">certifications</p>
+        <div class="project-container">
+            <div class='certification-container'>
+                
+            </div>
+        </div>`;
+        let certification = document.querySelector('.certification-container');
+        for(let i = 0; i < certifications.length; i++){
+            certification.innerHTML += `
+            <a href='${certifications[i].link}' target='_blank'>${certifications[i].title}</a>
+            `;
+        }
+    }
+}
+
+const showElement = (element) => {
+    element.style.display = 'block';
+}
+
+const hideElement = (element) => {
+    element.style.display = 'none';
+}
+
+window.onload = () => {
+    content.innerHTML = `<p class="title">${about_me[0].title}</p>
+        <p>${about_me[0].description}</p>`;
+}
