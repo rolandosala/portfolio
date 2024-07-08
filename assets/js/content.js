3264546ec353fb131b6001b8ac01c6b6d9a7915c
Roland <rolandosalajr@gmail.com>
2024-07-08 10:04:12 +0800

    Fixed images

--- assets/js/content.js
+++ assets/js/content.js
@@ -14,23 +14,26 @@ const knowledge = [
         title: 'Backend-End Development',
         description: 'Basic understanding and familiarity of concepts on storing data and authentication.',
         list: [ 
-            { name: 'Express', icon: 'https://img.icons8.com/color/48/express-js.png' , alt: 'html-5--v1'},
+           /*  { name: 'Express', icon: 'https://img.icons8.com/color/48/express-js.png' , alt: 'html-5--v1'}, */
             { name: 'PHP', icon: 'https://img.icons8.com/ios-filled/50/php-logo.png' , alt: 'css3'},
             { name: 'MySQL', icon: 'https://img.icons8.com/fluency/48/mysql-logo.png' , alt: 'javascript--v1'},
-            { name: 'Google Cloud Platform', icon: 'https://img.icons8.com/color/48/google-cloud.png' , alt: 'vuejs'},
-            { name: 'Bootstrap', icon: 'https://img.icons8.com/color-glass/48/bootstrap.png' , alt: 'bootstrap'},
+            /* { name: 'Google Cloud Platform', icon: 'https://img.icons8.com/color/48/google-cloud.png' , alt: 'vuejs'},
+            { name: 'Bootstrap', icon: 'https://img.icons8.com/color-glass/48/bootstrap.png' , alt: 'bootstrap'}, */
         ]
     },
     {
-        title: 'Application Programming Interface',
+        title: 'Web and Graphic Design',
         description: 'Basic understanding and familiarity on testing request and response using API using the tool below.',
-        list: ['Postman']
+        list: [
+            { name: 'Photoshop', icon: 'https://img.icons8.com/color/48/adobe-photoshop--v1.png' , alt: 'Photoshop'},
+            { name: 'Figma', icon: 'https://img.icons8.com/color/48/figma--v1.png' , alt: 'Figma'},
+        ]
     },
-    {
+    /* {
         title: 'Cloud Computing',
         description: 'Basic understanding and familiarity of the technologies stated below.',
         list: ['Google Cloud Platform', 'Firebase']
-    },
+    }, */
 ];
 
 const about_me = [
@@ -44,37 +47,46 @@ const certifications = [
     {
         link: 'https://drive.google.com/file/d/1TSlIYFAuw2oBWCaTRUYED9V97XPy3NOL/view?usp=drive_link',
         title: 'Basic Level of Cloud Computing',
-        image: 'blob/main/assets/img/BasicCloudComputing.png'
     },
     {
         link: 'https://drive.google.com/file/d/1TTmG-Y0238WUYkeE6d1luIKC24xCIfKd/view?usp=drive_link',
         title: 'Basic Level of Software Engineering',
-        image: 'blob/main/assets/img/BasicLevelofSoftwareEngineering.png'
     },
     {
         link: 'https://drive.google.com/file/d/1qc7fK1Ckz-FgGJ59EI_QWS2V2aiMhBpk/view?usp=drive_link',
         title: 'Intermediate Level of Software Engineering',
-        image: 'blob/main/assets/img/IntermediateLevelofSoftwareEngineering.png'
     },
     {
         link: 'https://drive.google.com/file/d/1fCxXPUG91ukUOITQmyuslw8Gz1cQlBUe/view?usp=drive_link',
-        title: 'Advance Level of Software Engineering',
-        image: 'blob/main/assets/img/AdvancedLevelofSoftwareEngineering.png'
+        title: 'Advanced Level of Software Engineering',
     },
     {
         link: 'https://drive.google.com/file/d/1fFTYrLPH6AeAGo0R3IWoXc_uR36cZYfI/view?usp=drive_link',
         title: 'Principles of Web Development and Introduction to HTML',
-        image: 'blob/main/assets/img/PrinciplesofWebDevelopment.png'
     },
     {
         link: 'https://drive.google.com/file/d/1_ya9Lve4ivxLNIFUIErMLNNxxuJ6a3Ah/view?usp=drive_link',
         title: 'Using HTML and CSS to Design a Website',
-        image: 'blob/main/assets/img/UsingHTMLandCSS.png'
     },
     {
         link: 'https://drive.google.com/file/d/16hrcisk_S6m4kgKHBdgUGJo_CK6YEkdJ/view?usp=drive_link',
         title: 'Basic Javascript for Web Development',
-        image: 'blob/main/assets/img/BasicJavascriptfor WebDevelopment.png'
+    },
+    {
+        link: 'https://drive.google.com/file/d/1XDBF9NB5UpiXnc6Xa3PX6NHCg_s5Apr-/view?usp=drive_link',
+        title: 'Developing a Static Website',
+    },
+    {
+        link: 'https://drive.google.com/file/d/121tuBVvRhYnrbLLMjctVXfRxb7f4hvKD/view?usp=drive_link',
+        title: 'Microsoft Productivity Tools',
+    },
+    {
+        link: 'https://drive.google.com/file/d/18KiJf4HdDJAVeqUfpj2MHT2gqGHDg_rb/view?usp=drive_link',
+        title: 'Navigating Google Workspace',
+    },
+    {
+        link: 'https://drive.google.com/file/d/1DUz6UoX64f6IVr3j9XmYBK-JMDicITwq/view?usp=drive_link',
+        title: 'Programming for Intermediate Users Using Python',
     }
 ];
 
